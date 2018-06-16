3d7c67ffb589f5d1a77e96270e79ca955293c436
Hanna Paranhos <hannapp10@gmail.com>
2018-06-16 19:47:26 -0300

    Telaassembleia
    N consegui deixar o combobox no formato padrão e nem os textos da tabela na cor branca, se alguem puder olhar

--- assembleia.php
+++ assembleia.php
@@ -64,28 +64,51 @@
                 <!-- contact info -->
                 <div class="col-md-5 col-md-offset-1 contact-info col-push">
                     <div class="section-title left">
-                        <h2>Contact us</h2>
+                        <h1>Pesquisa de presença </h1>
                     </div>
-                    <p>Cras ex mauris, ornare eget pretium sit amet, dignissim et turpis. Nunc nec maximus dui, vel suscipit dolor. Donec elementum velit a orci facilisis rutrum. </p>
-                    <h3 class="mt60">Main Office</h3>
-                    <p class="con-item">C/ Libertad, 34 <br> 05200 ArÃ©valo </p>
-                    <p class="con-item">0034 37483 2445 322</p>
-                    <p class="con-item">hello@company.com</p>
+                    
+                    <select id="assembleia">
+                        <option>Escolha a assembleia:</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                    </select>
+                    <br><br>
+                    <table class = "table">
+                         <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">Nome</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>Mark</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td>Jacob</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">3</th>
+                              <td>Larry</td>
+                            </tr>
+                          </tbody>
+
+                    </table>
                 </div>
                 <!-- contact form -->
                 <div class="col-md-6 col-pull">
                     <form class="form-class" id="con_form">
                         <div class="row">
-                            <div class="col-sm-6">
-                                <input type="text" name="name" placeholder="Your name">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" name="email" placeholder="Your email">
+                            <div class="section-title left"> <h1>Cadastro de Assembleia</h1></div>
+                            <h2>Data:</h2><br><div class="col-sm-6">
+                               
+                                <input type="date" name="data_assembleia">
                             </div>
                             <div class="col-sm-12">
-                                <input type="text" name="subject" placeholder="Subject">
-                                <textarea name="message" placeholder="Message"></textarea>
-                                <button class="site-btn">send</button>
+                                <button class="site-btn" name="cadastroassembleia">Cadastrar</button>
                             </div>
                         </div>
                     </form>
