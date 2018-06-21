3d38220bb13a7bbee06b082d8e6577d087397efe
Hanna Paranhos <hannapp10@gmail.com>
2018-06-21 18:21:46 -0300

    varias coisas

--- adm/ClassPessoa.php
+++ adm/ClassPessoa.php
@@ -7,17 +7,18 @@ use \PDOException;
 
 require_once "Database.php";
 
-class pessoa{
+class Pessoa{
     private $p_id;
     private $p_cpf;
     private $p_rg;
     private $p_nome;
     private $p_tipo;
     private $p_carteirinha;
-    private $p_nasciento;
+    private $p_nascimento;
     private $p_celular;
     private $p_apelido;
     private $p_faculdade;
+    private $rep_id;
 
     public function __construct()
     {
@@ -28,6 +29,7 @@ class pessoa{
 
     public function setId($value){
         $this->p_id = $value;
+        return 1;
     }
     public function setCpf($value){
         $this->p_cpf = $value;
@@ -48,7 +50,7 @@ class pessoa{
         $this->p_carteirinha = $value;
     }
 
-    public function setNascimentos($value){
+    public function setNasc($value){
         $this->p_nascimento = $value;
     }
     public function setCelular($value){
@@ -60,19 +62,23 @@ class pessoa{
     public function setFaculdade($value){
         $this->p_faculdade = $value;
     }
+    public function setRepID($value){
+        $this->rep_id = $value;
+    }
 
     public function insert(){
         try{
-            $stmt = $this->conn->prepare("INSERT INTO `pessoa`(`p_cpf`,`p_rg`,`p_nome`,`p_tipo`,`p_carteirinha`, `p_nascimento`,`p_celular`,`p_apelido`,`p_faculdade`) VALUES(:p_cpf, :p_rg, :p_nome, :p_tipo,:p_carteirinha,:p_nascimento,:p_celular,:p_apelido,:p_faculdade)");
+            $stmt = $this->conn->prepare("INSERT INTO `pessoa`(`p_cpf`,`p_rg`,`p_nome`,`p_tipo`,`p_carteirinha`, `p_nascimento`,`p_celular`,`p_apelido`,`p_faculdade`,`rep_id`) VALUES(:p_cpf, :p_rg, :p_nome, :p_tipo,:p_carteirinha,:p_nascimento,:p_celular,:p_apelido,:p_faculdade,:rep_id)");
             $stmt->bindParam(":p_cpf", $this->p_cpf);
             $stmt->bindParam(":p_rg", $this->p_rg);
             $stmt->bindParam(":p_nome", $this->p_nome);
-            $stmt->bindParam(":p_tipo", $this->patri_multa);
+            $stmt->bindParam(":p_tipo", $this->p_tipo);
             $stmt->bindParam(":p_carteirinha", $this->p_carteirinha);
-            $stmt->bindParam(":p_nascimento", $this->p_nasciento);
+            $stmt->bindParam(":p_nascimento", $this->p_nascimento);
             $stmt->bindParam(":p_celular", $this->p_celular);
             $stmt->bindParam(":p_apelido", $this->p_apelido);
             $stmt->bindParam(":p_faculdade", $this->p_faculdade);
+            $stmt->bindParam(":rep_id", $this->rep_id);
             $stmt->execute();
             return 1;
         }catch(PDOException $e){
@@ -129,3 +135,5 @@ class pessoa{
         return $stmt;
     }
 }*/
+
+  ?>
--- pessoas.php
+++ pessoas.php
@@ -1,171 +1,213 @@
+<?php
+
+use Rumon\Database\Pessoa;
+
+require_once 'adm/ClassPessoa.php';
+
+$pessoa = new Pessoa();
+
+if (isset($_POST['cadastrarpessoa'])) {
+    $pessoa->setCpf($_POST['cpfpessoa']);
+    $pessoa->setRg($_POST['rgpessoa']);
+    $pessoa->setNome($_POST['nomepessoa']);
+    $pessoa->setTipo($_POST['tipopessoa']);
+    $pessoa->setCarteirinha($_POST['carteirinhapessoa']);
+    $pessoa->setNasc($_POST['nascpessoa']);
+    $pessoa->setCelular($_POST['celpessoa']);
+    $pessoa->setApelido($_POST['apelidopessoa']);
+    $pessoa->setFaculdade($_POST['facupessoa']);
+    $pessoa->setRepID($_POST['rep_id']);
+    $pessoa->insert();
+    echo 'foi';
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-	<title>Rumon</title>
-	<meta charset="UTF-8">
-	<meta name="description" content="Labs - Design Studio">
-	<meta name="keywords" content="lab, onepage, creative, html">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Favicon -->
-	<link href="img/favicon.ico" rel="shortcut icon"/>
+    <head>
+        <title>Rumon</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Labs - Design Studio">
+        <meta name="keywords" content="lab, onepage, creative, html">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="shortcut icon"/>
 
-	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,700" rel="stylesheet">
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,700" rel="stylesheet">
 
-	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="css/font-awesome.min.css"/>
-	<link rel="stylesheet" href="css/flaticon.css"/>
-	<link rel="stylesheet" href="css/owl.carousel.css"/>
-	<link rel="stylesheet" href="css/style.css"/>
-
-
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-</head>
-<body>
-	<!-- Page Preloder -->
-	<div id="preloder">
-		<div class="loader">
-			<img src="img/logo_rumon.png" alt="">
-			<h2>Loading.....</h2>
-		</div>
-	</div>
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/font-awesome.min.css"/>
+        <link rel="stylesheet" href="css/flaticon.css"/>
+        <link rel="stylesheet" href="css/owl.carousel.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
 
 
-	<!-- Header section -->
-	<header class="header-section">
-		<div class="logo">
-			<img src="img/logo_rumon.png" alt=""><!-- Logo -->
-		</div>
-		<!-- Navigation -->
-		<div class="responsive"><i class="fa fa-bars"></i></div>
-		<nav>
-			<ul class="menu-list">
-				<li><a href="home.html">Home</a></li>
-				<li><a href="rep.php">República</a></li>
-				<li class="active"><a href="pessoas.php">Pessoas</a></li>
-				<li><a href="assembleia.php">Assembleia</a></li>
-				<li><a href="patrimonio.php">Patrimônio</a></li>
-			</ul>
-		</nav>
-	</header>
-	<!-- Header section end -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+        <!-- Page Preloder -->
+        <div id="preloder">
+            <div class="loader">
+                <img src="img/logo_rumon.png" alt="">
+                <h2>Loading.....</h2>
+            </div>
+        </div>
+
+
+        <!-- Header section -->
+        <header class="header-section">
+            <div class="logo">
+                <img src="img/logo_rumon.png" alt=""><!-- Logo -->
+            </div>
+            <!-- Navigation -->
+            <div class="responsive"><i class="fa fa-bars"></i></div>
+            <nav>
+                <ul class="menu-list">
+                    <li><a href="home.html">Home</a></li>
+                    <li><a href="rep.php">República</a></li>
+                    <li class="active"><a href="pessoas.php">Pessoas</a></li>
+                    <li><a href="assembleia.php">Assembleia</a></li>
+                    <li><a href="patrimonio.php">Patrimônio</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!-- Header section end -->
         <br><br><br>
 
-	 <!-- Contact section -->
-    <div class="contact-section spad fix">
-        <div class="container">
-            
-            <div class="col-md-12">
-                <!-- contact info -->
-                <div class="col-md-6">
-                    <div class="section-title left">
-                        <h1>Pesquisa Pessoa</h1>
-                    </div>
-                    
-                    <form class="form-class" id="con_form">
-                        
-                            
+        <?php
+        if (isset($result)) {
+            ?>
+            <div class="alert alert-success">
+                <?php echo $result; ?>
+            </div>
+            <?php
+        } else if (isset($error)) {
+            ?>
+            <div class="alert alert-danger">
+                <?php echo $error; ?>
+            </div>
+            <?php
+        }
+        ?>
+
+        <!-- Contact section -->
+        <div class="contact-section spad fix">
+            <div class="container">
+
+                <div class="col-md-12">
+                    <!-- contact info -->
+                    <div class="col-md-6">
+                        <div class="section-title left">
+                            <h1>Pesquisa Pessoa</h1>
+                        </div>
+
+                        <form class="form-class" id="con_form">
+
+
                             <h2>Digite o nome:</h2><br>
-                               
-                                <input type="text" name="pesquisarnomepessoa" class="col-md-3">
-                            
+
+                            <input type="text" name="pesquisarnomepessoa" class="col-md-3">
+
                             <div class="text-right">
                                 <button class="site-btn" name="botaopesquisarpessoa">Pesquisar</button>
                             </div>
-                        
-                    </form>
-                    
-                    
-                    <table class="table" style="background-color: white;">
-                      <thead>
-                        <tr>
-                          <th>Firstname</th>
-                          <th>Lastname</th>
-                          <th>Email</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>John</td>
-                          <td>Doe</td>
-                          <td>john@example.com</td>
-                        </tr>
-                        <tr>
-                          <td>Mary</td>
-                          <td>Moe</td>
-                          <td>mary@example.com</td>
-                        </tr>
-                        <tr>
-                          <td>July</td>
-                          <td>Dooley</td>
-                          <td>july@example.com</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                </div>
-                <!-- contact form -->
-                <div class="col-md-6">
-                    <form class="form-class" id="con_form">
-                        <div class="row">
-                            <div class="section-title left"><h1>Cadastro de Pessoa</h1></div>
-                            <h2>CPF:</h2>
+
+                        </form>
+
+
+                        <table class="table" style="background-color: white;">
+                            <thead>
+                                <tr>
+                                    <th>Firstname</th>
+                                    <th>Lastname</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>John</td>
+                                    <td>Doe</td>
+                                    <td>john@example.com</td>
+                                </tr>
+                                <tr>
+                                    <td>Mary</td>
+                                    <td>Moe</td>
+                                    <td>mary@example.com</td>
+                                </tr>
+                                <tr>
+                                    <td>July</td>
+                                    <td>Dooley</td>
+                                    <td>july@example.com</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- contact form -->
+                    <div class="col-md-6">
+                        <form class="form-class" id="con_form" method="post">
+                            <div class="row">
+                                <div class="section-title left"><h1>Cadastro de Pessoa</h1></div>
+                                <h2>CPF:</h2>
                                 <input type="text" name="cpfpessoa">
-                            <h2>RG:</h2>
+                                <h2>RG:</h2>
                                 <input type="text" name="rgpessoa">
-                            <h2>Nome:</h2>
+                                <h2>Nome:</h2>
                                 <input type="text" name="nomepessoa">
-                            <h2>Tipo(Calouro/Morador/Decano):</h2>
+                                <h2>Tipo(Calouro/Morador/Decano):</h2>
                                 <input type="text" name="tipopessoa">
-                            <h2>Carteirinha</h2>
-                                <input type="text" name="tipopessoa">
-                            <h2>Data:</h2>
-                            <input type="date" name="nascpessoa">
-                            <h2>Celular:</h2>
+                                <h2>Carteirinha</h2>
+                                <input type="text" name="carteirinhapessoa">
+                                <h2>Data:</h2>
+                                <input type="date" name="nascpessoa">
+                                <h2>Celular:</h2>
                                 <input type="text" name="celpessoa">
-                            <h2>Apelido:</h2>
+                                <h2>Apelido:</h2>
                                 <input type="text" name="apelidopessoa">
-                            <h2>Faculdade</h2>
+                                <h2>Faculdade</h2>
                                 <input type="text" name="facupessoa">
-                                
-                                <select id="reppessoa">
+
+                                <select id="reppessoa" name="rep_id">
                                     <option>República:</option>
                                     <option>1</option>
                                     <option>2</option>
                                     <option>3</option>
                                 </select>
-                                
-                                <button class="site-btn" name="cadastrarpessoa">Cadastrar</button>
-                            </div>
-                        </div>
-                    </form>
+
+                                <button class="site-btn" name="cadastrarpessoa">Cadastrar</button>  
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
-    
+    </div>
+
     <!-- Contact section end-->
 
 
-	<!-- Footer section -->
-	<footer class="footer-section">
-		<h2>2017 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
-	</footer>
-	<!-- Footer section end -->
+    <!-- Footer section -->
+    <footer class="footer-section">
+        <h2>2017 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
+    </footer>
+    <!-- Footer section end -->
 
 
 
-	<!--====== Javascripts & Jquery ======-->
-	<script src="js/jquery-2.1.4.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/magnific-popup.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/circle-progress.min.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0YyDTa0qqOjIerob2VTIwo_XVMhrruxo"></script>
-	<script src="js/map.js"></script>
-	<script src="js/main.js"></script>
+    <!--====== Javascripts & Jquery ======-->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/magnific-popup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/circle-progress.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0YyDTa0qqOjIerob2VTIwo_XVMhrruxo"></script>
+    <script src="js/map.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
